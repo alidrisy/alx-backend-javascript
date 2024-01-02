b7451b41770d54597bb5bb69d278ff567a65a481
alidrisy <a.alidrisy6@gmail.com>
2024-01-02 22:29:14 +0300

    3. Parameter defaults

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     const task = true;
---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,1 @@
+export default const getSumOfHoods = (initialNumber, expansion1989 = 89, expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
