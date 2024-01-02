431dc00e1d016a9b9ebf6e4799bc4229c1cab07c
alidrisy <a.alidrisy6@gmail.com>
2024-01-02 22:14:28 +0300

    fix style

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -3,11 +3,11 @@ export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
-  
+
 export function getLast() {
   return ' is okay';
 }
-  
+
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
