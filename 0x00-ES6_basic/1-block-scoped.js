a2335bfa76f22b5ec89a1de9925c5b7189a84451
alidrisy <a.alidrisy6@gmail.com>
2024-01-02 21:13:22 +0300

    Const or Let

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+    var task = false;
+    var task2 = true;
+  
+    if (trueOrFalse) {
+      const task = true;
+      const task2 = false;
+    }
+  
+    return [task, task2];
+  }
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,3 @@
+# 0x00. ES6 Basics
+
+* This is the first project in the Specializations programe from ALX.
