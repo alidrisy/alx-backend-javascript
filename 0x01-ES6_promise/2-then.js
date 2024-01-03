74187e69f94fe75332fa8b9032e3530002f8de7c
alidrisy <a.alidrisy6@gmail.com>
2024-01-03 23:36:54 +0300

    2. Catch me if you can!

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,5 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
