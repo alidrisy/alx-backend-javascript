109a44f6cb156b251032a1627c2bac895eabe447
alidrisy <a.alidrisy6@gmail.com>
2024-01-04 01:07:59 +0300

    6. Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((value) => {
+      value.forEach((val) => ({
+        status: val.status,
+        value: val.status === 'fulfilled' ? val.value : String(val.reason),
+      }));
+    });
+}
