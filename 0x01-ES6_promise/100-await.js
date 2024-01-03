4ceb2ffe9de48a86e8c77f3dd9b831d031e7c553
alidrisy <a.alidrisy6@gmail.com>
2024-01-04 02:14:22 +0300

    10. Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (e) {
+    return { photo: null, user: null };
+  }
+}
