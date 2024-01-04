4ee8c34b40f415d01e85f3724a8c6d28f67f2ca9
alidrisy <a.alidrisy6@gmail.com>
2024-01-04 21:55:07 +0300

    7. Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,38 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val === 'string') {
+      this._name = val;
+    } else {
+      throw new TypeError('Name must be string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    if (typeof val === 'string') {
+      this._code = val;
+    } else {
+      throw new TypeError('Code must be string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
