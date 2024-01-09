32249189efcbdeb1defde7988000013386dcf90a
alidrisy <a.alidrisy6@gmail.com>
2024-01-09 16:13:24 +0300

    10. More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,7 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+  for (const [key, val] of map) {
+    if (val === 1) map.set(key, 100);
+  }
+  return map;
+}
