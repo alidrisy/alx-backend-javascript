bc8882982eed6e990db73632d040e2f30345a9d2
alidrisy <a.alidrisy6@gmail.com>
2024-01-09 21:08:55 +0300

    11. Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+export const weakMap = new WeakMap();
+export function queryAPI(endPoint) {
+  if (weakMap.has(endPoint)) {
+    const val = weakMap.get(endPoint);
+    if (val < 5) {
+      weakMap.set(endPoint, val + 1);
+    } else {
+      throw new Error('Endpoint load is high');
+    }
+  } else {
+    weakMap.set(endPoint, 1);
+  }
+}
