4178615bd1ada00a5a23259d7a0b62be5430f1e7
alidrisy <a.alidrisy6@gmail.com>
2024-02-28 02:19:21 +0300

    6. Create a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (_req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`This app listening on port ${port}`);
+});
