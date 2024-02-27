bcaac6345b1632e9d7ba6e3f9009b589ecf5c711
alidrisy <a.alidrisy6@gmail.com>
2024-02-28 01:49:40 +0300

    5. Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -26,9 +26,7 @@ const countStudents = (path) => new Promise((resolve, reject) => {
       }
       const respons = [];
       respons.push(`Number of students: ${students.length}`);
-      console.log(`Number of students: ${students.length}`);
       for (const [field, group] of Object.entries(studentsFields)) {
-        console.log(`Number of students in ${field}: ${group.length}. List: ${group.join(', ')}`);
         respons.push(`Number of students in ${field}: ${group.length}. List: ${group.join(', ')}`);
       }
       resolve(respons);
@@ -36,22 +34,22 @@ const countStudents = (path) => new Promise((resolve, reject) => {
   });
 });
 
-
 const HOST = '127.0.0.1';
 const PORT = 1245;
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    if(req.url === '/') {
-        res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-        countStudents("database.csv")
-        .then((report) => {
-          res.end(`This is the list of our students\n${report.join('\n')}`);
-        }).catch((err) => {
-          res.end(err instanceof Error ? err.message : err.toString());
-        })
-    }
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents('database.csv')
+      .then((report) => {
+        res.end(`This is the list of our students\n${report.join('\n')}`);
+      }).catch((err) => {
+        const error = err instanceof Error ? err.message : err.toString();
+        res.end(`This is the list of our students\n${error}`);
+      });
+  }
 });
 
 app.listen(PORT, HOST, () => {
