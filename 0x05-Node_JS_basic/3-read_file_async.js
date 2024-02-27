7eb0f262dc48c8405ae8550196bbd75e60c84b7d
alidrisy <a.alidrisy6@gmail.com>
2024-02-28 01:18:01 +0300

    3. Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -9,21 +9,21 @@ const countStudents = (path) => new Promise((resolve, reject) => {
       const fileLines = data.toString('utf-8').split('\n');
       const keys = fileLines[0].split(',');
       const students = [];
-      const studentsFields = {}
+      const studentsFields = {};
       for (const student of fileLines.slice(1)) {
         if (student.length) {
           const listStudent = student.split(',');
           students.push(Object.fromEntries(
-            keys.map((key, index) => [key, listStudent[index]])
+            keys.map((key, index) => [key, listStudent[index]]),
           ));
           const field = listStudent[listStudent.length - 1];
           if (!Object.keys(studentsFields).includes(field)) {
             studentsFields[field] = [];
           }
-          studentsFields[field].push(listStudent[0])
+          studentsFields[field].push(listStudent[0]);
         }
       }
-      console.log('Number of students: ' + students.length);
+      console.log(`Number of students: ${students.length}`);
       for (const [field, group] of Object.entries(studentsFields)) {
         console.log(`Number of students in ${field}: ${group.length}. List: ${group.join(', ')}`);
       }
---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const HOST = '127.0.0.1';
+const PORT = 1245;
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at: http://${HOST}:${PORT}/`);
+});
+
+module.exports = app;
