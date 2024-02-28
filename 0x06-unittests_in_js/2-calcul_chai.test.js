fe0d28ca1460b28ec31a7e8f22d71a17aaa2a072
alidrisy <a.alidrisy6@gmail.com>
2024-02-29 00:00:04 +0300

    2. Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -4,62 +4,62 @@ const calculateNumber = require("./2-calcul_chai.js");
 describe('calculateNumber', function () {
   describe('SUM', function () {
     it('should return 4', function () {
-      expect(calculateNumber('SUM', 1, 3)).to.be(4);
+      expect(calculateNumber('SUM', 1, 3)).to.equal(4);
     });
     it('should return 5', function () {
-      expect(calculateNumber('SUM', 1, 3.7)).to.be(5);
+      expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
     });
     it('should return 6', function () {
-      expect(calculateNumber('SUM', 1.5, 3.7)).to.be(6);
+      expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
     });
     it(`checking if numbers round`, function() {
-      expect(calculateNumber('SUM', 0, 0)).to.be(0);
+      expect(calculateNumber('SUM', 0, 0)).to.equal(0);
     });
     it(`checking if numbers round`, function() {
-      expect(calculateNumber('SUM', -1.6, -1.7)).to.be(-4);
+      expect(calculateNumber('SUM', -1.6, -1.7)).to.equal(-4);
     });
     it(`checking if numbers round`, function() {
-      expect(calculateNumber('SUM', -1.4, -1.3)).to.be(-2);
+      expect(calculateNumber('SUM', -1.4, -1.3)).to.equal(-2);
     });
   });
   describe('SUBTRACT', function () {
     it('should return -2', function () {
-      expect(calculateNumber('SUBTRACT', 1, 3)).to.be(-2);
+      expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
     });
     it('should return -3', function () {
-      expect(calculateNumber('SUBTRACT', 1, 3.7)).to.be(-3);
+      expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
     });
     it('should return -2', function () {
-      expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.be(-2);
+      expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-2);
     });
     it(`checking if numbers round`, function() {
-      expect(calculateNumber('SUBTRACT', 0, 0)).to.be(0);
+      expect(calculateNumber('SUBTRACT', 0, 0)).to.equal(0);
     });
     it(`checking if numbers round`, function() {
-      expect(calculateNumber('SUBTRACT', -1.6, -1.7)).to.be(0);
+      expect(calculateNumber('SUBTRACT', -1.6, -1.7)).to.equal(0);
     });
     it(`checking if numbers round`, function() {
-      expect(calculateNumber('SUBTRACT', -1.4, -1.3)).to.be(0);
+      expect(calculateNumber('SUBTRACT', -1.4, -1.3)).to.equal(0);
     });
   });
   describe('DIVIDE', function () {
     it('should return 2', function () {
-      expect(calculateNumber('DIVIDE', 4, 2)).to.be(2);
+      expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
     });
     it('should return 2', function () {
-      expect(calculateNumber('DIVIDE', 3.7, 2)).to.be(2);
+      expect(calculateNumber('DIVIDE', 3.7, 2)).to.equal(2);
     });
     it('should return 0.5', function () {
-      expect(calculateNumber('DIVIDE', 1.5, 3.7)).to.be(0.5);
+      expect(calculateNumber('DIVIDE', 1.5, 3.7)).to.equal(0.5);
     });
     it(`checking if numbers round`, function() {
-      expect(calculateNumber('DIVIDE', 0, 0)).to.be('Error');
+      expect(calculateNumber('DIVIDE', 0, 0)).to.equal('Error');
     });
     it(`checking if numbers round`, function() {
-      expect(calculateNumber('DIVIDE', -1.6, -1.7)).to.be(1);
+      expect(calculateNumber('DIVIDE', -1.6, -1.7)).to.equal(1);
     });
     it(`checking if numbers round`, function() {
-      expect(calculateNumber('DIVIDE', -1.4, -1.3)).to.be(1);
+      expect(calculateNumber('DIVIDE', -1.4, -1.3)).to.equal(1);
     });
   });
 });
