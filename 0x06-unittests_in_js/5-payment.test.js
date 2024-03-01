f10ea3eabbdd91bd90f55a0ea76260d8b5f38986
alidrisy <a.alidrisy6@gmail.com>
2024-03-01 18:16:17 +0300

    5. Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,49 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  describe('sendPaymentRequestToApi', () => {
+    let calculateNumberStub;
+    let consoleLogSpy;
+
+    beforeEach(() => {
+      consoleLogSpy = sinon.spy(console, 'log');
+      calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    });
+
+    afterEach(() => {
+      calculateNumberStub.restore();
+      consoleLogSpy.restore();
+    });
+
+    it('Call sendPaymentRequestToAPI with 100, and 20 and get return val = 10', () => {
+      calculateNumberStub.returns(10)
+      sendPaymentRequestToApi(100, 20);
+
+      expect(calculateNumberStub.calledOnce).to.be.true;
+      expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+      expect(consoleLogSpy.calledOnce).to.be.true;
+      expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
+    });
+
+    it('Call sendPaymentRequestToAPI with 100, and 20 and get return val = 120', () => {
+      sendPaymentRequestToApi(100, 20);
+
+      expect(calculateNumberStub.calledOnce).to.be.true;
+      expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+      expect(consoleLogSpy.calledOnce).to.be.true;
+      expect(consoleLogSpy.calledWith('The total is: 120')).to.be.true;
+    });
+
+    it('Call sendPaymentRequestToAPI with 10, and 10 and get return val = 20', () => {
+      sendPaymentRequestToApi(10, 10);
+
+      expect(calculateNumberStub.calledOnce).to.be.true;
+      expect(calculateNumberStub.calledWith('SUM', 10, 10)).to.be.true;
+      expect(consoleLogSpy.calledOnce).to.be.true;
+      expect(consoleLogSpy.calledWith('The total is: 20')).to.be.true;
+    });
+  });
+});
