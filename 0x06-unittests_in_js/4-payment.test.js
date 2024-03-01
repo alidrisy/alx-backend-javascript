29ca73f1eabe6cd5c1beea62e0718e32530c0711
alidrisy <a.alidrisy6@gmail.com>
2024-03-01 17:47:19 +0300

    4. Stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,14 +6,21 @@ describe('sendPaymentRequestToApi', () => {
   describe('sendPaymentRequestToApi', () => {
     let sendPaymentRequestToApi;
     let calculateNumberSpy;
+    let calculateNumberStub;
+    let consoleLogSpy;
 
     beforeEach(() => {
       calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+      consoleLogSpy = sinon.spy(console, 'log');
+      calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+      calculateNumberStub.returns(10);
       sendPaymentRequestToApi = require('./3-payment');
     });
 
     afterEach(() => {
       calculateNumberSpy.restore();
+      calculateNumberStub.restore();
+      consoleLogSpy.restore();
     });
 
     it('should call Utils.calculateNumber with the correct arguments', () => {
@@ -21,6 +28,8 @@ describe('sendPaymentRequestToApi', () => {
 
       expect(calculateNumberSpy.calledOnce).to.be.true;
       expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+      expect(consoleLogSpy.calledOnce).to.be.true;
+      expect(consoleLogSpy.calledWith('The total is: 120')).to.be.true;
     });
   });
 });
