c9e08175c8608622aa8b801589c2daab63d7a8ee
alidrisy <a.alidrisy6@gmail.com>
2024-03-02 17:13:43 +0300

    8. Organize a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,30 +1,32 @@
 const readDatabase = require('../utils');
 
 class StudentsController {
-  static getAllStudents (request, response) {
+  static getAllStudents(request, response) {
     readDatabase(process.argv[2])
       .then((studentsFields) => {
         const sortedKeys = Object.keys(studentsFields).sort();
         const res = ['This is the list of our students'];
         for (const key of sortedKeys) {
-          res.push(`Number of students in ${field}: ${studentsFields[key].length}. List: ${studentsFields[key].join(', ')}`);
+          res.push(`Number of students in ${key}: ${studentsFields[key].length}. List: ${studentsFields[key].join(', ')}`);
         }
-        return response.status(200).send(res.join('\n'));
-      }).catch((err) => {
-        return response.status(500).send('Cannot load the database');
+        response.status(200).send(res.join('\n'));
+      }).catch(() => {
+        response.status(500).send('Cannot load the database');
       });
   }
 
-  static getAllStudentsByMajor (request, response) {
+  static getAllStudentsByMajor(request, response) {
     const { major } = request.params;
     if ((major !== 'CS') && (major !== 'SWE')) {
-      return response.status(500).send('Major parameter must be CS or SWE');
+      response.status(500).send('Major parameter must be CS or SWE');
     }
     readDatabase(process.argv[2])
       .then((studentsFields) => {
-        return response.status(200).send(`List: ${studentsFields[major].join(', ')}`);
-      }).catch((err) => {
-        return response.status(500).send('Cannot load the database');
-    });
+        response.status(200).send(`List: ${studentsFields[major].join(', ')}`);
+      }).catch(() => {
+        response.status(500).send('Cannot load the database');
+      });
   }
 }
+
+module.exports = StudentsController;
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,26 +1,26 @@
 const fs = require('fs');
 
-const readDatabase = (path) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-        if (err) {
-          reject(new Error('Cannot load the database'));
-        }
-        if (data) {
-          const fileLines = data.toString('utf-8').split('\n');
-          const studentsFields = {};
-          for (const student of fileLines.slice(1)) {
-            if (student.length) {
-              const listStudent = student.split(',');
-              const field = listStudent[listStudent.length - 1];
-              if (!Object.keys(studentsFields).includes(field)) {
-                studentsFields[field] = [];
-              }
-              studentsFields[field].push(listStudent[0]);
-            }
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (data) {
+      const fileLines = data.toString('utf-8').split('\n');
+      const studentsFields = {};
+      for (const student of fileLines.slice(1)) {
+        if (student.length) {
+          const listStudent = student.split(',');
+          const field = listStudent[listStudent.length - 1];
+          if (!Object.keys(studentsFields).includes(field)) {
+            studentsFields[field] = [];
           }
-          resolve(studentsFields);
+          studentsFields[field].push(listStudent[0]);
         }
-    });
-};
+      }
+      resolve(studentsFields);
+    }
+  });
+});
 
 module.exports = readDatabase;
