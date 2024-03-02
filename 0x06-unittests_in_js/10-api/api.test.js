6e9c86bbc240b859b9aeb7d0bb5d67b04ccf310b
alidrisy <a.alidrisy6@gmail.com>
2024-03-02 15:38:00 +0300

    fix errors

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -34,79 +34,20 @@ describe('API integration test', () => {
     });
   });
 
-  describe("Available_payments page", function() {
-    it("check correct status for correct url", function() {
-	    request.get("http://localhost:7865/available_payments", (err, res, body) => {
-	      if (err) {
-		      expect(res.statusCode).to.not.equal(200);
-	      } else {
-		     expect(res.statusCode).to.equal(200);
-	      }
-	    });
-    });
-    it("check correct body content for correct url", function() {
-	    const option = {json: true};
-	    const payLoad = {
-	      payment_methods: {
-		      credit_cards: true,
-		      paypal: false
-	      }
-	    }
-	    request.get("http://localhost:7865/available_payments", option, (err, res, body) => {
-	      if (err) {
-		      expect(res.statusCode).to.not.equal(200);
-	      } else {
-		      expect(body).to.deep.equal(payLoad);
-	      }
-	    });
+  it('POST /login returns valid response', (done) => {
+    request.post(`${API_URL}/login`, {json: {userName: 'Pinkbrook'}}, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome Pinkbrook');
+      done();
     });
   });
-  
-  describe("Login", function() {
-    it("check correct status code for request that's sent properly", function(done) {
-	    const opt = {
-	      url: "http://localhost:7865/login",
-	      json: true,
-	      body: {
-		      userName: 'JOE'
-	      }
-	    };
-	    request.post(opt, function(err, res, body) {
-	      expect(res.statusCode).to.equal(200);
-	      done();
-	    });
-    });
 
-    it("check correct content for request that's sent properly", function(done) {
-      const opts = {
-          url: "http://localhost:7865/login",
-          json: true,
-          body: {
-            userName: 'JOE'
-          }
-	    };
-      request.post(opts, function(err, res, body) {
-        if (err) {
-          expect(res.statusCode).to.not.equal(200);
-        } else {
-          expect(body).to.contain('Welcome JOE');
-        }
-        done();
-      });
-    });
-    
-    it("check correct status code for request that's not sent properly", function(done) {
-      const op = {
-        url: "http://localhost:7865/login",
-          json: true,
-          body: {
-            usame: 'JOE'
-          }
-      };
-      request.post(op, function(err, res, body) {
-        expect(res.statusCode).to.equal(404);
-        done();
-      });
+  it('GET /available_payments returns valid response', (done) => {
+    request.get(`${API_URL}/available_payments`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(JSON.parse(body))
+        .to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}});
+      done();
     });
   });
 });
